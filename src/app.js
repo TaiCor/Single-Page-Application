8cb0b56932bc8f88b091f0a4b50a49dd40fae58a
Vadim Kapustin <vkapustin@msteam.local>
2017-07-24 15:57:55 +0700

    .

--- src/app.js
+++ src/app.js
@@ -1,1 +1,1 @@
-console.log('Hello');
+import angular from 'angular';
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
     entry: './src/app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
 }
