837f315f4ea78ff81dab4f4190af6efc4edcac0d
Vadim Kapustin <vkapustin@msteam.local>
2017-07-24 14:38:31 +0700

    .

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
     entry: './src/app.js',
-    output:{
+    output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
