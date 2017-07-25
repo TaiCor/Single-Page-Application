c8878ad07940c974a3bf48e3cc7494d3c66274dd
Vadim Kapustin <vkapustin@msteam.local>
2017-07-25 11:36:16 +0700

    set conf for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: './src/app.js',
@@ -8,7 +8,38 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+        {
+           test: /\.css$/,
+           use: ExtractTextPlugin.extract({
+           fallback: "style-loader",
+           use: "css-loader"
+           })
+        },
+        {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader'
+         ]
+        },
+        {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader'
+         ]
+        },
+        {
+         test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+          'file-loader'
+         ]
+        }        
+        ]
+    },
     plugins: [
+        new ExtractTextPlugin("styles.css"),
         new HtmlWebpackPlugin({
             template: 'src/index.html'
         })
