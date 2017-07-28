808276a9d4fe8646644b31c25e73c9e5977e93d0
Vadim Kapustin <vkapustin@msteam.local>
2017-07-28 14:44:53 +0700

    Rename photos to post

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import angular from 'angular'
 import 'angular-material'
 import 'angular-material/angular-material.min.css'
 
-import photos from './photos.component/photos.component'
+import photos from './posts/posts.component'
 
 angular.module('fotoalbum', ['ngMaterial'])
   .component('photos', photos)
--- src/photos.component/photos.component.js
+++
@@ -1,7 +0,0 @@
-import controller from './photos.controller'
-import template from './photos.template.html'
-
-export default {
-  controller,
-  template
-}
