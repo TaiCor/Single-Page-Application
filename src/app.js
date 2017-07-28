edd54cb5523fa92781a96201a58d0535fd29b96d
Vadim <foxcorer@gmail.com>
2017-07-28 05:17:57 +0300

    .

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import angular from 'angular'
 import 'angular-material'
 import 'angular-material/angular-material.min.css'
-import './photos.component/photos.module'
+// import './photos.component/photos.module'
 import './style.css'
+import phoneCatComponent from './phone.cat/phone.cat.component'
 
-angular.module('fotoalbum', ['ngMaterial', 'photosModule'])
+// angular.module('fotoalbum', ['ngMaterial', 'photosModule'])
+angular.module('phoneCat', ['ngMaterial'])
+.component('phonecatcomponent', phoneCatComponent)
---
+++ src/phone.cat/phone.cat.component.js
@@ -0,0 +1,7 @@
+import phoneCatcontroller from './phone.cat.controller'
+import phoneCatTemplate from './phone.cat.template.html'
+
+export default {
+  template: phoneCatTemplate,
+  controller: phoneCatcontroller
+}
