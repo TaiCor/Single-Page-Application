6b018783362d7cace552ca7c780206f400a4133d
Vadim Kapustin <vkapustin@msteam.local>
2017-07-28 18:01:20 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import angular from 'angular'
 import 'angular-material'
 import 'angular-material/angular-material.min.css'
-
-import photos from './posts/posts.component'
+import post from './post/post.component'
+import posts from './posts/posts.component'
 
 angular.module('fotoalbum', ['ngMaterial'])
-  .component('photos', photos)
+  .component('posts', posts)
+  .component('post', post)
