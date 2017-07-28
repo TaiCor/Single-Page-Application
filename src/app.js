b3072f35d862865456fbb443bb10a9b4a065ef5f
Vadim Kapustin <vkapustin@msteam.local>
2017-07-28 14:38:25 +0700

    Fix rebase bugs

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 import angular from 'angular'
 import 'angular-material'
 import 'angular-material/angular-material.min.css'
+
+import photos from './photos.component/photos.component'
+
+angular.module('fotoalbum', ['ngMaterial'])
+  .component('photos', photos)
