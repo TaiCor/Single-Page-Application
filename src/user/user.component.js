2e37dee4e2b340986fcacb542a4daff4e241d237
Vadim Kapustin <vkapustin@msteam.local>
2017-08-01 13:55:52 +0700

    write user list

---
+++ src/user/user.component.js
@@ -0,0 +1,10 @@
+import template from './user.template.html'
+import controller from './user.controller'
+
+export default {
+  binding: {
+    user: '='
+  },
+  controller,
+  template
+}
---
+++ src/user/user.controller.js
@@ -0,0 +1,5 @@
+export default class likeController {
+  $onInit () {
+    this.user = this.user
+  }
+}
