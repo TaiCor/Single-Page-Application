aadc68a87808b7054624a97cb0237a58a1af83d4
Vadim Kapustin <vkapustin@msteam.local>
2017-08-02 13:55:16 +0700

    add states

--- src/route.config.js
+++ src/route.config.js
@@ -1,10 +1,15 @@
-import allpostComponent from './posts/posts.component'
+import allPostComponent from './posts/posts.component'
+import myPostsComponent from './myposts/myposts.component'
 
 routes.$inject = ['$stateProvider']
 export default function routes ($stateProvider) {
   $stateProvider
     .state('allPosts', {
       url: '/allposts',
-      component: allpostComponent
+      component: allPostComponent
+    })
+    .state('myPosts', {
+      url: '/myposts',
+      component: myPostsComponent
     })
 }
