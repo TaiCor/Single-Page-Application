634cc071ec1ad2c996ef6a72dd41f63b95667498
Vadim <foxcorer@gmail.com>
2017-08-20 07:42:06 +0300

    add newpost

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import angular from 'angular'
+import 'angular-ui-router'
+import 'ng-file-upload'
 import './style.css'
 import 'angular-material'
 import 'angular-material/angular-material.min.css'
@@ -7,20 +9,21 @@ import posts from './posts/posts.component'
 import myPosts from './myposts/myposts.component'
 import servicePost from './posts/posts.service'
 import serviceMyPost from './myposts/myposts.service'
-import 'angular-ui-router'
 import route from './route.config'
 import serviceComments from './comments/comments.service'
 import comments from './comments/comments.component'
 import login from './login/login.component'
 import singup from './singup/singup.component'
+import newpost from './newpost/newpost.component'
 
-angular.module('fotoalbum', ['ngMaterial', 'ui.router'])
+angular.module('fotoalbum', ['ngMaterial', 'ui.router', 'ngFileUpload'])
   .component('posts', posts)
   .component('post', post)
   .component('myPosts', myPosts)
   .component('comments', comments)
   .component('login', login)
   .component('singup', singup)
+  .component('newpost', newpost)
   .service('servicePost', servicePost)
   .service('serviceMyPost', serviceMyPost)
   .service('serviceComments', serviceComments)
---
+++ src/newpost/newpost.component.js
@@ -0,0 +1,7 @@
+import template from './newpost.template.html'
+import controller from './newpost.controller'
+
+export default {
+  template,
+  controller
+}
