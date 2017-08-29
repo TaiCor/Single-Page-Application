97068261cf03f0feb83297205138c01f06b59de3
Vadim Kapustin <vkapustin@msteam.local>
2017-08-29 18:55:09 +0700

    edit

--- src/myposts/dialog/dialog.controller.js
+++ src/myposts/dialog/dialog.controller.js
@@ -8,7 +8,6 @@ export default class dialogController {
   addPost () {
     this.serviceNewPost.uploadPic(this.picFile, this.data.title, this.data.description)
     .then((res) => {
-      console.log(res)
       this.responce = res.data
       this.$mdDialog.hide(res.data)
     })
