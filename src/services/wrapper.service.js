d03b32dcdc7ed861c548add52f9bf64394d6a7f6
Vadim <foxcorer@gmail.com>
2017-09-07 18:35:23 +0300

    create wrapper service for $http

---
+++ src/services/wrapper.service.js
@@ -0,0 +1,16 @@
+export default class wrapperService {
+  constructor ($http) {
+    this.url = 'http://localhost:3000/'
+    this.$http = $http
+  }
+  wrap (query, data, method = 'POST') {
+    return this.$http({
+      method,
+      url: this.url + query,
+      data,
+      config: {
+        withCredentials: true
+      }
+    })
+  }
+}
